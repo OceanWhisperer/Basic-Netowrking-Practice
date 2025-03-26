db4a7e0f66f4706f833d0c7ed80292d09b050631
OceanWhisperer <arnavmahajan630@gmail.com>
2025-03-26 19:20:21 +0530

    got interfaces and  their private ips

--- The Very Basics/getinterfacesandpublicip.cpp
+++ The Very Basics/getinterfacesandpublicip.cpp
@@ -1,3 +1,6 @@
+// BUILD WITH g++ getinterfacesandpublicip.cpp -o getip -lcurl
+// EXECUTE WITH ./ip
+
 extern "C" {
     #include <stdio.h>
     #include <stdlib.h>
@@ -13,7 +16,28 @@ extern "C" {
 }
 
 void get_interface_and_ips() {
-    
+    struct ifaddrs *interfaces, *ifa;
+    char ip[INET6_ADDRSTRLEN];
+    if(getifaddrs(&interfaces) == -1) {
+        perror("Error");
+        return;
+    }
+    for(ifa = interfaces; ifa!=NULL; ifa=ifa->ifa_next) {
+        int family = ifa->ifa_addr->sa_family;
+        if(family == AF_INET) {
+            void * addr = &((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+            inet_ntop(AF_INET, addr, ip, sizeof ip);
+            printf("Interface: %s | IPv4: %s\n", ifa->ifa_name, ip);
+        }
+        else {
+            void *addr = &((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+            inet_ntop(AF_INET6, addr, ip, sizeof ip);
+            printf("Interface: %s | IPv6: %s\n", ifa->ifa_name, ip);
+        }
+    }
+    freeifaddrs(interfaces);
+    return;
+
 }
 
 
@@ -34,7 +58,9 @@ void get_public_ip() {
 
 
 int main() {
-
+    printf("Listing all the Network interfaces and their private Ip addresses: ..\n");
+    get_interface_and_ips();
+    printf("\n");
     printf("The public IP is: ");
     get_public_ip();
     return 0;
