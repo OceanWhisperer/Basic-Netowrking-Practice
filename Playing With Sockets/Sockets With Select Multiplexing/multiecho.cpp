cfbb5a5e73681301a907bf2761a565f2505896f6
OceanWhisperer <arnavmahajan630@gmail.com>
2025-04-03 23:05:38 +0530

    learned Timeout Implemented Maintainence, removes inactive clients. Max chat history feature added

--- Playing With Sockets/Sockets With Select Multiplexing/multiecho.cpp
+++ Playing With Sockets/Sockets With Select Multiplexing/multiecho.cpp
@@ -11,8 +11,29 @@
 #define PORT 12345
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
-
+# define MAX_CHATS 100
 using namespace std;
+
+void remove_inactive(unordered_map<int,time_t>&last_active, unordered_map<int, string>&clients) {
+    time_t now = time(NULL);
+    auto it = last_active.begin(); 
+
+    while (it != last_active.end()) {  
+        if (now - it->second > 30) {
+            int client_fd = it->first;
+             
+                cout << clients[client_fd] << " 30 seconds idle. Disconnected" << endl;
+                close(client_fd);
+                clients.erase(it->first);
+            
+
+            it = last_active.erase(it);
+        }       
+        else {
+            ++it;
+        }
+    }
+}
 void broadcastHistory(vector<string>&chat_history, int client) {
    for(const auto c : chat_history) {
      send(client, c.c_str(), c.length(), 0);
@@ -46,7 +67,8 @@ int main() {
     struct sockaddr_in server_addy, client_addy;
     socklen_t client_address = sizeof(client_addy);
     fd_set monitor_fds;
-    unordered_map<int , string> clients; 
+    unordered_map<int , string> clients;
+    unordered_map<int, time_t>last_active;
     char buffer[BUFFER_SIZE];
     
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -82,15 +104,20 @@ int main() {
         FD_SET(server_fd, &monitor_fds);
         int max_fd = server_fd;
 
+        struct timeval tv = {30, 0};
+
         for(auto &val : clients)  { 
             FD_SET(val.first, &monitor_fds);
             if(val.first > max_fd) max_fd = val.first;
         }
          
-        int activity = select(max_fd + 1, &monitor_fds, NULL, NULL, NULL);
+        int activity = select(max_fd + 1, &monitor_fds, NULL, NULL, &tv);
         if(activity < 0) {
             perror("Select call Failed");
             return 1;
+        }
+        if(activity == 0) {
+            remove_inactive(last_active, clients);
         }
         
         if(FD_ISSET(server_fd, &monitor_fds)) {
@@ -111,6 +138,7 @@ int main() {
                 if(bytes_recv <= 0) {
                     cout << "Client " + it->second + " has disconnected" << endl;
                     close(client_fd);
+                    last_active.erase(client_fd);
                     it = clients.erase(it);
                     continue;
                 }
@@ -124,26 +152,30 @@ int main() {
                         len--; // the username is by default stored with \n charecter so we trim it. 
                     }
                     clients[client_fd] = string(buffer);
+                    last_active[client_fd] = time(NULL);
                     string welcome_msg = clients[client_fd] + " has joined the chat.\n";
                     cout << welcome_msg << endl;
                     int total_clients = clients.size();
                     total_clients--;
                     chat_history.push_back(welcome_msg);
+                    if(chat_history.size() > MAX_CHATS)chat_history.erase(chat_history.begin());
                     if(total_clients < chat_history.size()) {
                         broadcastHistory(chat_history, client_fd);
                     }
                     else {
                         broadcastMessage(clients, welcome_msg, client_fd);
                     }
-                } else {
+                } 
+                else {
                     int len = strlen(buffer);
                     while (len > 0 && (buffer[len - 1] == '\n' || buffer[len - 1] == '\r')) {
                         buffer[len - 1] = '\0';
                         len--; // the normal message is also by default stored with \n.
                     }
-                              
+                    last_active[client_fd] = time(NULL);         
                     string echo = clients[client_fd] + ": " + buffer + "\n";
                     chat_history.push_back(echo);
+                    if(chat_history.size() > MAX_CHATS)chat_history.erase(chat_history.begin());
                     cout << echo << endl;
                     broadcastMessage(clients, echo, client_fd);
                 }
